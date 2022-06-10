fa32659beb1a96580a9ea3387347139d54724b33
sedron <jfabianabadd@gmail.com>
2022-06-10 13:34:40 -0500

    Tested router

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+  import Router, {location} from 'svelte-spa-router';
+  import Home from './lib/Pages/Home.svelte';
+  import Empresa from './lib/Pages/Empresa.svelte';
+</script>
+
+<Router routes={{
+  '/': Home,
+  '/about': Empresa,
+}}
+/>
